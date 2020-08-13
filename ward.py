7e727c6f598d33bc8e38168a061c0b1e4ac3238b
Adithi Satish <45226748+adithisatish@users.noreply.github.com>
2020-08-13 11:46:14 +0530

    organised repo

--- ward.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = requests.get("https://en.wikipedia.org/wiki/List_of_wards_in_Bangalore").text
-soup = BeautifulSoup(url,'lxml')
-#print(soup.prettify())
-table = soup.find("table",{'class':'wikitable'})
-print(table)
